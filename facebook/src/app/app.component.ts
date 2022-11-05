03e7b166199f52d10b8233417a312bebe04ee46d
viki2701 <viki2727eee@gmail.com>
2022-11-05 12:53:05 +0530

    card work on 05.11.2022

--- facebook/src/app/app.component.ts
+++ facebook/src/app/app.component.ts
@@ -14,16 +14,30 @@ export class AppComponent {
       profileImage:'assets/facebook/People/Elon Musk.jpg',
       postImage:'assets/facebook/Tesla Car.jpg',
       likedBy:'R Vignesh',
-      postedTime : '1 h'
+      postedTime : '1 h',
 
     },
     {
-      profileName : 'Elon Musk',
-      profileImage:'assets/facebook/People/Elon Musk.jpg',
-      postImage:'assets/facebook/Tesla Car.jpg',
-      likedBy:'R Vignesh',
-      postedTime : '1 h'
-    }
+      profileName : 'Mark Zuckerberg',
+      profileImage:'assets/facebook/People/Mark Zuckerberg.jpg',
+      postImage:'assets/facebook/sidebar icon/Post image/facebook post.jpg',
+      likedBy:'Nikola Tesla',
+      postedTime : '2 h',
+    },
+    {
+      profileName : 'Bill Gates',
+      profileImage:'assets/facebook/People/Bill Gates.jpg',
+      postImage:'assets/facebook/sidebar icon/Post image/post image 2.jpg',
+      likedBy:'Mahindra Singh Dhoni',
+      postedTime : '3 h',
+    },
+    {
+      profileName : 'Nikola Tesla',
+      profileImage:'assets/facebook/People/Nikola Tesla.jpg',
+      postImage:'assets/facebook/sidebar icon/Post image/post image 3.jpg',
+      likedBy:'Jack Ma',
+      postedTime : '4 h',
+    },
   ];
 
 
