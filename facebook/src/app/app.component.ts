7e87fbc356ac752010b3bf7438814a2436147069
Gowtham <https://bitbucket.org/mannai_mobility/fid_service-center/commits/>
2022-11-05 08:17:37 +0300

    FIx multiple

--- facebook/src/app/app.component.ts
+++ facebook/src/app/app.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'facebook';
+
+  post = [
+    {
+      profileName : 'Elon Musk',
+      profileImage:'assets/facebook/People/Elon Musk.jpg',
+      postImage:'assets/facebook/Tesla Car.jpg',
+      likedBy:'R Vignesh',
+      postedTime : '1 h'
+
+    },
+    {
+      profileName : 'Elon Musk',
+      profileImage:'assets/facebook/People/Elon Musk.jpg',
+      postImage:'assets/facebook/Tesla Car.jpg',
+      likedBy:'R Vignesh',
+      postedTime : '1 h'
+    }
+  ];
+
+
+
 }
