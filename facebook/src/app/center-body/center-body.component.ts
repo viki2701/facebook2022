e586b79c468b5c5d02ad4b025e51a10007865cb6
viki2701 <viki2727eee@gmail.com>
2022-11-06 15:42:53 +0530

    compount of app work finsh

--- facebook/src/app/center-body/center-body.component.ts
+++ facebook/src/app/center-body/center-body.component.ts
@@ -12,4 +12,60 @@ export class CenterBodyComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  post = [
+    {
+      profileName : 'Elon Musk',
+      profileImage:'assets/facebook/People/Elon Musk.jpg',
+      postImage:'assets/facebook/Tesla Car.jpg',
+      likedBy:'R Vignesh',
+      postedTime : '1 h',
+      postMessage:''
+
+
+    },
+    {
+      profileName : 'Mark Zuckerberg',
+      profileImage:'assets/facebook/People/Mark Zuckerberg.jpg',
+      postImage:'assets/facebook/sidebar icon/Post image/facebook post.jpg',
+      likedBy:'Nikola Tesla',
+      postedTime : '2 h',
+      postMessage:''
+
+    },
+    {
+      profileName : 'Bill Gates',
+      profileImage:'assets/facebook/People/Bill Gates.jpg',
+      postImage:'assets/facebook/sidebar icon/Post image/post image 2.jpg',
+      likedBy:'Mahindra Singh Dhoni',
+      postedTime : '3 h',
+      postMessage:''
+
+    },
+    {
+      profileName : 'Nikola Tesla',
+      profileImage:'assets/facebook/People/Nikola Tesla.jpg',
+      postImage:'assets/facebook/sidebar icon/Post image/post image 3.jpg',
+      likedBy:'Jack Ma',
+      postedTime : '4 h',
+      postMessage:''
+    },
+    {
+      profileName : 'Mahindra Singh Dhoni',
+      profileImage:'assets/facebook/People/Mahindra Singh Dhoni.jpg',
+      postImage:'',
+      likedBy:'Mahindra Singh Dhoni',
+      postedTime : '3 h',
+      postMessage:'Hi, How are you'
+    },
+    {
+      profileName : 'Iron Man',
+      profileImage:'assets/facebook/People/Iron Man.jpg',
+      postImage:'',
+      likedBy:'Jack Ma',
+      postedTime : '4 h',
+      postMessage:'Hi, How are you?.......'
+
+    }
+  ];
+
 }
--- facebook/src/app/right-sidebar/right-sidebar.component.ts
+++ facebook/src/app/right-sidebar/right-sidebar.component.ts
@@ -12,4 +12,57 @@ export class RightSidebarComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  contact = [
+
+    {
+      contactName:'Abdul Kalam',
+      contactImage:'assets/facebook/People/Abdul Kalam.png',
+      contactOnline:'',
+    },
+    {
+      contactName:'Bil Gates',
+      contactImage:'assets/facebook/People/Bill Gates.jpg',
+      contactOnline:'active',
+    },
+    {
+      contactName:'Elon Musk',
+      contactImage:'assets/facebook/People/Elon Musk.jpg',
+      contactOnline:'active',
+    },
+    {
+      contactName:'Jeff Bezons',
+      contactImage:'assets/facebook/People/Jeff Bezons.jpg',
+      contactOnline:'active',
+    },
+    {
+      contactName:'Iron Man',
+      contactImage:'assets/facebook/People/Iron Man.jpg',
+    },
+    {
+      contactName:'Jack Ma',
+      contactImage:'assets/facebook/People/Jack Ma.jpg',
+      contactOnline:'active',
+    },
+    {
+      contactName:'Mahindra Singh Dhoni',
+      contactImage:'assets/facebook/People/Mahindra Singh Dhoni.jpg',
+      contactOnline:'active',
+    },
+    {
+      contactName:'Mark Zuckerberg',
+      contactImage:'assets/facebook/People/Mark Zuckerberg.jpg',
+      contactOnline:'active',
+    },
+    {
+      contactName:'Mukesh Ambani',
+      contactImage:'assets/facebook/People/Mukesh Ambani.jpg',
+      contactOnline:'active',
+    },
+    {
+      contactName:'Nikola Tesla',
+      contactImage:'assets/facebook/People/Nikola Tesla.jpg',
+      contactOnline:'',
+    },
+];
+
 }
